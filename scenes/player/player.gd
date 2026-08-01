c3fdaffc27ca9d36b1a498f4204f71f76760591b
Ehab Aijaz <clutch.10306@gmail.com>
2026-08-01 11:53:03 +0530

    trying to debug this watering can BARELYWORKING

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,10 +4,15 @@ extends CharacterBody2D
 @onready var tool_state_machine : AnimationNodeStateMachinePlayback = $AnimationTree.get('parameters/ToolStateMachine/playback')
 
 var direction : Vector2
+var last_direction : Vector2
 var speed := 300
 var can_move := true
+signal tool_use(tool :Tools, pos: Vector2)
 enum Tools {HOE, AXE, WATER}
 var current_tool = Tools.AXE
+var tool_direction_offset : int = 12
+var tool_y_offset : int = 4
+
 const tool_connection = {
 	Tools.HOE: 'hoe',
 	Tools.AXE: 'axe',
@@ -17,6 +22,8 @@ const tool_connection = {
 func _physics_process(delta: float) -> void:
 	if can_move:
 		get_input()
+	if direction:
+		last_direction = direction
 	velocity = direction * speed * int(can_move)
 	move_and_slide()
 	animation()
@@ -29,13 +36,20 @@ func get_input():
 	direction = Input.get_vector("left","right","up","down")
 	
 	if Input.is_action_just_pressed('action'):
+		print("--- STEP 1: Action Pressed. Current Tool is: ", current_tool)
 		tool_state_machine.travel(tool_connection[current_tool])
 		$AnimationTree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 		can_move = false
-	var tool_direction = Input.get_axis('tool_backward', 'tool_forward') as int
-	if Input.is_action_just_pressed('tool_forward') or Input.is_action_just_pressed("tool_backward"):
-		current_tool = posmod(current_tool + tool_direction, Tools.size()) as Tools
-
+		await $AnimationTree.animation_finished
+		print("--- STEP 2: Animation finished. Emitting signal...")
+		tool_use.emit(current_tool, position + last_direction * tool_direction_offset + Vector2(0,tool_y_offset))
+		
+	if Input.is_action_just_pressed("tool_forward"):
+		current_tool = (current_tool + 1) % Tools.size() as Tools
+		print("Selected Tool: ", current_tool)
+	elif Input.is_action_just_pressed("tool_backward"):
+		current_tool = (current_tool - 1 + Tools.size()) % Tools.size() as Tools
+		print("Selected Tool: ", current_tool)
 
 func animation():
 	if direction:
